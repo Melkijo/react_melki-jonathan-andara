45a003dedc0bff90a8daa5ba2dbedff0b146d85a
Melkijo <jonathanandaraa@gmail.com>
2023-03-14 13:10:50 +0800

    chore: update figma task

---
+++ 15_Event Handling/praktikum/practice-lms-alterra/fomulir-buku/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import FormulirPage from "./pages/formulirPage";
+function App() {
+  return (
+    <>
+      <FormulirPage />
+    </>
+  );
+}
+
+export default App;
---
+++ 15_Event Handling/praktikum/practice-lms-alterra/fomulir-buku/src/pages/FormulirPage.jsx
@@ -0,0 +1,23 @@
+export default function FormulirPage() {
+  return (
+    <>
+      <h1>Formulir Buku Baru</h1>
+      <form id="fomrBuku">
+        <label>Judul</label>
+        <br />
+        <input type="text" name="" id="" />
+        <br />
+        <label>Pengarang</label>
+        <br />
+        <input type="text" name="" id="" />
+        <br />
+        <label htmlFor="">Cetakan</label>
+        <br />
+        <input type="text" name="" id="" />
+        <br />
+        <input type="submit" value="Submit" />
+        <input type="reset" value="Reset" />
+      </form>
+    </>
+  );
+}
